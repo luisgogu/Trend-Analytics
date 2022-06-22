1015394340d1e3e405b97204f440fdabe835b158
Gonzalo Córdova Pou <71346949+gonzalo-cordova-pou@users.noreply.github.com>
2022-06-22 19:08:37 +0200

    Delete front/pruebas_gonzalo directory

--- front/pruebas_gonzalo/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField 
-from wtforms.validators import DataRequired
-
-class Todo(FlaskForm):
-    content = TextAreaField(validators=[DataRequired()])
-    submit = SubmitField('Submit todo')
